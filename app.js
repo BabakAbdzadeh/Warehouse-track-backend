266978d148c4b9dd1a3ed67e006d1bef016dd427
Babak abdolahzadeh <49559487+BabakAbdzadeh@users.noreply.github.com>
2022-06-13 17:08:46 +0000

    "combined app.js and api.js"

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const _ = require('lodash');
 const db = require('./db.js');
 
 
+// API setup
+const json = express.json();
+app.use(express.json());
+const Item = db.Item;
+const Delete = db.Delete;
+
 
 //  ------------ Modules setups ------------
 
@@ -198,6 +204,122 @@ function objectValidFinder(inputObject) {
   };
   return update;
 };
+
+
+
+// -------------- API ------------------------
+
+// Get index, fetch all
+
+app.route("/api/inventory")
+
+.get((req, res) => {
+  Item.find((err, results) => {
+    if (!err) {
+      res.send(results);
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+// Get  fetch specific
+.post((req, res) => {
+  const item = new Item({
+    barcode: req.body.barcode,
+    quantity: req.body.quantity,
+    floor: req.body.floor,
+    shelf: req.body.shelf,
+    level: req.body.level,
+
+    product: req.body.product,
+    brand: req.body.brand,
+    name: req.body.name
+  }, );
+  item.save((err) => {
+    if (!err) {
+      res.send("Successfully added a new item to inventory");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
+app.put("/api/inventory/:id", (req, res) => {
+
+  const itemId = req.params.id;
+  const submitedUpdates = req.body;
+  console.log(itemId);
+  console.log(submitedUpdates);
+
+  delete submitedUpdates.id;
+
+  const update = {};
+  for (const key in submitedUpdates) {
+    if (submitedUpdates[key] !== "" || submitedUpdates[key] === String) {
+
+      update[key] = _.startCase(submitedUpdates[key]);
+    } else if (submitedUpdates[key] !== "" || submitedUpdates[key] === Number) {
+      update[key] = submitedUpdates[key];
+    }
+
+  };
+  Item.findByIdAndUpdate(itemId, update, {new: true}, (err, result) =>
+  {
+    if (!err) {
+      console.log("Updated, results:"+ update);
+       
+      
+    
+      
+    } else {
+      console.log(err);
+    }
+  });
+  res.send("Successfully added a new item to inventory");
+    
+
+});
+
+
+app.route("/api/inventory/:id/:comment")
+
+.delete((req, res) => {
+  const id = req.params.id;
+  const comment= req.params.comment;
+
+  
+
+  Item.findById(id, (err, results) => {
+    if (!err) {
+      const deletedItem = new Delete({
+        comment: comment,
+        item: results
+      });
+      console.log(deletedItem);
+      deletedItem.save();
+
+
+    } else {
+      console.log(err);
+    }
+  });
+  Item.findByIdAndRemove(id, (err, deleted) => {
+    if (!err) {
+      console.log(`${deleted} has been deleted`)
+       if (!err) {
+      res.send("Successfully deleted");
+    } else {
+      res.send(err);
+    }
+    } else {
+      console.log(err)
+    };
+  });
+});
+
+
 
 
 
